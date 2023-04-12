b26d764d977c014544db99eadd9cb4a72900578a
Gabriel Luz <luz.gabriel010@gmail.com>
2023-04-12 18:34:35 -0300

    adicionando site

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+setTimeout(playSong,5000);
+function playSound(){
+    var sound = document.getElementById("sound");
+    sound.play();
+}
+function playSong(){
+    var song = document.getElementById("song");
+    song.play();
+}
